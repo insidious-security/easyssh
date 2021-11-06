8acb6c25fa0c230c837948f3b81e1f68aa314238
sidious <25933899+insidious-security@users.noreply.github.com>
2021-11-07 00:35:22 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,6 @@ printf "Creating container..\n"
 docker build -t easyssh . > /dev/null 2>&1
 printf "Starting container, running config.\n"
 docker run -dit --name easyssh -p 2107:22 easyssh:latest /bin/bash > /dev/null 2>&1
-timeout 25s docker logs --follow easyssh 
+timeout 40s docker logs --follow easyssh 
 printf "\n"
 
