d0a8046997e954079aab4cbdf1d75a7151fccb09
sidious <25933899+insidious-security@users.noreply.github.com>
2021-11-12 19:47:55 +0100

    Update easyssh.sh

--- easyssh.sh
+++ easyssh.sh
@@ -35,7 +35,7 @@ msg(){
 rootperm(){
   if [ "$(id -u)" -ne 0 ]; then
     error "You must be root"
-        exit 1
+    exit 1
   fi
 }
 
@@ -49,61 +49,61 @@ sshcheck(){
 }
 
 sshdcop(){
-    cp /etc/ssh/sshd_config /etc/ssh/backup.sshd_config
-    cat <<EOT >/etc/ssh/sshd_config    
-    Protocol 2
-    IgnoreRhosts yes
-    HostbasedAuthentication no
-    PermitRootLogin no
-    PermitEmptyPasswords no
-    X11Forwarding no
-    MaxAuthTries 5
-    ClientAliveInterval 900
-    ClientAliveCountMax 0
-    Subsystem sftp internal-sftp
-    UsePAM yes
-    HostKey /etc/ssh/ssh_host_ed25519_key
-    HostKey /etc/ssh/ssh_host_rsa_key
-    KexAlgorithms curve25519-sha256@libssh.org
-    Ciphers chacha20-poly1305@openssh.com,aes256-gcm@openssh.com,aes128-gcm@openssh.com,aes256-ctr,aes192-ctr,aes128-ctr
-    MACs hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com,umac-128-etm@openssh.com
+  cp /etc/ssh/sshd_config /etc/ssh/backup.sshd_config
+  cat <<EOT >/etc/ssh/sshd_config    
+  Protocol 2
+  IgnoreRhosts yes
+  HostbasedAuthentication no
+  PermitRootLogin no
+  PermitEmptyPasswords no
+  X11Forwarding no
+  MaxAuthTries 5
+  ClientAliveInterval 900
+  ClientAliveCountMax 0
+  Subsystem sftp internal-sftp
+  UsePAM yes
+  HostKey /etc/ssh/ssh_host_ed25519_key
+  HostKey /etc/ssh/ssh_host_rsa_key
+  KexAlgorithms curve25519-sha256@libssh.org
+  Ciphers chacha20-poly1305@openssh.com,aes256-gcm@openssh.com,aes128-gcm@openssh.com,aes256-ctr,aes192-ctr,aes128-ctr
+  MACs hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com,umac-128-etm@openssh.com
 EOT
-    #systemctl restart sshd.service && systemctl status sshd.service > /dev/null 2>&1  
-    /etc/init.d/ssh restart > /dev/null 2>&1  
+  #systemctl restart sshd.service && systemctl status sshd.service > /dev/null 2>&1  
+  /etc/init.d/ssh restart > /dev/null 2>&1  
     
-    if [ $? -eq 0 ]; then
-        msg "The SSH docker port is forwarded to: 2107"
-        sshd_chk=1
-    else
-        error "sshd_config file could not be hardened, check user permissions.. exiting.." && exit 1
-    fi
+  if [ $? -eq 0 ]; then
+      msg "The SSH docker port is forwarded to: 2107"
+      sshd_chk=1
+  else
+      error "sshd_config file could not be hardened, check user permissions.. exiting.." && exit 1
+  fi
 }
 
 setperm(){
-    chown root:root /etc/ssh/sshd_config
-    chmod 600 /etc/ssh/sshd_config
-    chown root:root /etc/anacrontab
-    chmod og-rwx /etc/anacrontab
-    chown root:root /etc/crontab
-    chmod og-rwx /etc/crontab
-    chown root:root /etc/cron.hourly
-    chmod og-rwx /etc/cron.hourly
-    chown root:root /etc/cron.daily
-    chmod og-rwx /etc/cron.daily
-    chown root:root /etc/cron.weekly
-    chmod og-rwx /etc/cron.weekly
-    chown root:root /etc/cron.monthly
-    chmod og-rwx /etc/cron.monthly
-    chown root:root /etc/cron.d
-    chmod og-rwx /etc/cron.d
-    chown root:root /etc/passwd
-    chmod 644 /etc/passwd
-    chown root:root /etc/group
-    chmod 644 /etc/group
-    chown root:root /etc/shadow
-    chmod 600 /etc/shadow
-    chown root:root /etc/gshadow
-    chmod 600 /etc/gshadow
+  chown root:root /etc/ssh/sshd_config
+  chmod 600 /etc/ssh/sshd_config
+  chown root:root /etc/anacrontab
+  chmod og-rwx /etc/anacrontab
+  chown root:root /etc/crontab
+  chmod og-rwx /etc/crontab
+  chown root:root /etc/cron.hourly
+  chmod og-rwx /etc/cron.hourly
+  chown root:root /etc/cron.daily
+  chmod og-rwx /etc/cron.daily
+  chown root:root /etc/cron.weekly
+  chmod og-rwx /etc/cron.weekly
+  chown root:root /etc/cron.monthly
+  chmod og-rwx /etc/cron.monthly
+  chown root:root /etc/cron.d
+  chmod og-rwx /etc/cron.d
+  chown root:root /etc/passwd
+  chmod 644 /etc/passwd
+  chown root:root /etc/group
+  chmod 644 /etc/group
+  chown root:root /etc/shadow
+  chmod 600 /etc/shadow
+  chown root:root /etc/gshadow
+  chmod 600 /etc/gshadow
 }
 
 
