48a3d440caf0f344ec9994462830418f95e2aabb
sidious <25933899+insidious-security@users.noreply.github.com>
2022-02-02 19:16:25 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,4 @@ docker build -t easyssh . > /dev/null 2>&1
 printf "Starting container, running config \n"
 clear
 docker run -dit --name easyssh -p 2107:22 easyssh:latest /bin/bash > /dev/null 2>&1
-timeout 40s docker logs --follow easyssh 
-
-
+docker logs --follow easyssh
